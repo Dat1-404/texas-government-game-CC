1e894c57f9854a9d74150cedad31225d827850d9
Dat1_ <85589260+Dat1-404@users.noreply.github.com>
2023-01-21 04:33:04 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -19,19 +19,31 @@ if var1 == 1 :
   input("After demonstrating a good moral character and a lack of criminal history, you are to be tested on United States history and civics.")
   input("Luckily for you, you learned English before coming here, which you must know.")
   input("\nTHE AMERICAN CIVICS TEST\n")
-  input("Instructions: Answer the questions with the number assigned to them. For example, if the correct answer is '1. 10', you will answer with '1' and press enter.\n")
+  input("Instructions: Answer the questions with the number assigned to them. For example, if the correct answer is '1. 10', you will answer with '1' and press enter. You must get a 60% or higher to pass.\n")
+  answers = []
   score = 0
   answer = input("Question 1: How many branches are there?\n1. 1\n2. 2\n3. 3\n4. 4\n")
   if answer == "3" :
     score = score + 1
-    print("Correct!")
-  else :
-    print("Incorrect.")
+    answers.append(answer)
   answer = input("Question 2: How many amendments are in the Bill of Rights?\n1. 5\n2. 13\n3. 12\n4. 10\n")
   if answer == "4" :
     score = score + 1
-    print("Correct!")
-  else :
-    print("Incorrect.")
+    answers.append(answer)
+  answer = input("Question 3: In what year did the United States gain independence?\n1. 1778\n2. 1776\n3. 1789\n4. 1700\n")
+  if answer == "2" :
+    score = score + 1
+    answers.append(answer)
+  answer = input("Question 4: What country did the United States gain it's independence from?\n1. Great Britain\n2. France\n3. Portugal\n4. Spain\n")
+  if answer == "1" :
+    score = score + 1
+    answers.append(answer)
+  answer = input("Question54. When is Independence Day?\n1. August 2nd\n2. July 4th\n3. December 25th\n4. November 24th\n")
+  if answer == "2" :
+    score = score + 1
+    answers.append(answer)
+  answer = input("What is the national animal of the United States?\n1. Bald Eagle\n2. Armadillo\n3. Unicorn\n4. Bison\n")
+  if answer == "1" :
+    score = score + 1
+    answers.append(answer)
     
-  
